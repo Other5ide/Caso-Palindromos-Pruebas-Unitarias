d7158bd2ecd98941bff02ee07c9ef99d41481f68
Francisco <fcocacerescolihuil@gmail.com>
2025-04-25 20:10:05 -0400

    improved esPalindromo

--- src/main/java/Palindromo.java
+++ src/main/java/Palindromo.java
@@ -1,15 +1,22 @@
 public class Palindromo {
+    public static void main(String[] args) {
+    }
 
     public boolean esPalindromo(String cadena) {
-        String cadenaInvertida = "";
-
-        char[] caracteres = cadena.toCharArray();
-
-        for (int i = caracteres.length - 1; i >= 0; i--) {
-            cadenaInvertida += caracteres[i];
+        if (cadena == null || cadena.length() == 0) {
+            throw new IllegalArgumentException("La cadena no puede ser nula");
         }
+        String cleaned = cadena.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        int left = 0, right = cleaned.length() - 1;
 
-        return cadena.equals(cadenaInvertida);
+        while (left <= right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
 }
